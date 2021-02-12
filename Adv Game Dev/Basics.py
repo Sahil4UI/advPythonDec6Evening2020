fb37c480e9945d1b27b8f7d10062ac88704763f9
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2021-02-12 19:05:41 +0530

    Update Basics.py

--- Adv Game Dev/Basics.py
+++ Adv Game Dev/Basics.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame,random
 pygame.init()
 
 width = 1000
@@ -20,7 +20,7 @@ class Player(pygame.sprite.Sprite):
         self.rect.x=width/2
         self.rect.y = height-50
         self.moveX=0
-        
+        self.health=100
     def update(self):
         keyPress = pygame.key.get_pressed()
         if keyPress[pygame.K_RIGHT]:
@@ -31,10 +31,66 @@ class Player(pygame.sprite.Sprite):
             self.moveX =0
             
         self.rect.x += self.moveX
+        self.hit = pygame.sprite.groupcollide(playerGroup,enemyGroup,False,True)
+        if self.hit:
+            self.health -=5
+        self.playerHealth()
+    def playerHealth(self):
+        pygame.draw.rect(screen,green, [5,5,self.health ,25])
+    
+    def Shoot(self):
+        bulletX  = self.rect.x + self.image.get_width()/2
+        bulletY  = self.rect.y
+        bullet = Bullet(bulletX,bulletY)
+        bulletGroup.add(bullet)
+        all_sprites.add(bullet)
+    
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((50,50))
+        self.image.fill(black)
+        self.rect=self.image.get_rect()
+        self.rect.x=random.randint(0,width -50)
+        self.rect.y = random.randint(0,height-50)
+        self.moveY = random.randint(1,4)
         
+    def update(self,*args):
+        self.rect.y += self.moveY
+        if self.rect.y > height:
+            self.rect.y = random.randint(-height,0)
+
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        super().__init__()
+        self.image = pygame.Surface((6,14))
+        self.image.fill(blue)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.moveY=-3
+    
+    def update(self):
+        self.rect.y += self.moveY
+        
+        if self.rect.y<0:
+            self.kill()
+            
+    
 all_sprites = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
+playerGroup = pygame.sprite.Group()
+playerGroup.add(player)
+bulletGroup = pygame.sprite.Group()
+enemyGroup = pygame.sprite.Group()
+
+for i in range(20):
+    enemy = Enemy()
+    enemyGroup.add(enemy)
+    all_sprites.add(enemy)
 
 FPS = 100
 clock = pygame.time.Clock()
@@ -43,6 +99,12 @@ while True:
         if event.type==pygame.QUIT:
             pygame.quit()
             quit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.Shoot()
+    
+    hit = pygame.sprite.groupcollide(bulletGroup,enemyGroup,True,True)            
             
     screen.fill(white)
     all_sprites.draw(screen)
